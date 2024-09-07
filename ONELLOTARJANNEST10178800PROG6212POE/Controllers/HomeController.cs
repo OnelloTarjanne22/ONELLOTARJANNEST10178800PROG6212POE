43dca694a8e6ccbf888d804577cb44b6231f08d6
Travis Tarjanne <129750427+OnelloTarjanne22@users.noreply.github.com>
2024-09-07 20:36:42 +0200

    Initial Css Styling

--- ONELLOTARJANNEST10178800PROG6212POE/Controllers/HomeController.cs
+++ ONELLOTARJANNEST10178800PROG6212POE/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace ONELLOTARJANNEST10178800PROG6212POE.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Claim()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
