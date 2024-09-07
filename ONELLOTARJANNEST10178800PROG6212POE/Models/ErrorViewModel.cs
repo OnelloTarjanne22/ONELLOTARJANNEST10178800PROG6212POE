d6b63a0a5a6af628fce10674216e5c193d989240
Travis Tarjanne <129750427+OnelloTarjanne22@users.noreply.github.com>
2024-09-07 15:26:50 +0200

    Add project files.

---
+++ ONELLOTARJANNEST10178800PROG6212POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ONELLOTARJANNEST10178800PROG6212POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
