7a587bf88ba6e3325fbb9e89169ed28d05cdb317
Travis Tarjanne <129750427+OnelloTarjanne22@users.noreply.github.com>
2024-09-09 17:50:02 +0200

    Final styling

--- ONELLOTARJANNEST10178800PROG6212POE/Controllers/HomeController.cs
+++ ONELLOTARJANNEST10178800PROG6212POE/Controllers/HomeController.cs
@@ -30,6 +30,14 @@ namespace ONELLOTARJANNEST10178800PROG6212POE.Controllers
         {
             return View();
         }
+        public IActionResult Post()
+        {
+            return View();
+        }
+        public IActionResult Track()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- ONELLOTARJANNEST10178800PROG6212POE/wwwroot/js/site.js
+++ ONELLOTARJANNEST10178800PROG6212POE/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.getElementById('claimForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Claim submitted successfully!');
+});
 
-// Write your JavaScript code.
+document.getElementById('trackingForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Tracking request sent!');
+});
